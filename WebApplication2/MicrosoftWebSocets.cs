41117452c3e1f207118ae301457f748b89cad6db
es-pasha <pasha@eventsoft.com.ua>
2015-05-29 17:28:29 +0300

    Sample project

---
+++ WebApplication2/MicrosoftWebSocets.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+	public class MicrosoftWebSocets : WebSocketHandler
+	{
+		private static WebSocketCollection _clients = new WebSocketCollection();
+
+		public override void OnOpen()
+		{
+			_clients.Add(this);
+		}
+
+		public override void OnMessage(string url)
+		{
+			this.Send(DateTime.Now.ToShortTimeString() +  " count connections: " + _clients.Count.ToString());
+		}
+
+		public override void OnClose()
+		{
+			_clients.Remove(this);
+		}
+	}
+}
---
+++ WebApplication2/WebSocketsServer.ashx.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using Microsoft.Web.WebSockets;
+
+namespace WebApplication2
+{
+	/// <summary>
+	/// Summary description for WebSocketsServer
+	/// </summary>
+	public class WebSocketsServer : IHttpHandler
+	{
+		public void ProcessRequest(HttpContext context)
+		{
+			if (context.IsWebSocketRequest)
+			{
+				context.AcceptWebSocketRequest(new MicrosoftWebSocets());
+			}
+		}
+
+		public bool IsReusable
+		{
+			get
+			{
+				return false;
+			}
+		}
+	}
+}
